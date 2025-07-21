db72e116fc34bbc1b03869b4444260d368ccaaf0
jboss <justin.boss@gmail.com>
2025-07-21 12:02:29 -0400

    Trying to make the red squiggles go away

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
-import NewsletterSignupForm from './NewsletterSignupForm';
+import NewsletterSignupForm from "./NewsLetterSignupForm";
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-12 text-gray-300 bg-transparent">
+    <footer className='w-full mt-12 text-gray-300 bg-transparent'>
       {/* Newsletter Sign-Up Form */}
       <NewsletterSignupForm />
 
       {/* Contact Info Row */}
-      <div className="text-sm text-center md:text-left px-4 pb-6 flex flex-col md:flex-row justify-center items-center gap-4 md:gap-8 flex-wrap">
+      <div className='text-sm text-center md:text-left px-4 pb-6 flex flex-col md:flex-row justify-center items-center gap-4 md:gap-8 flex-wrap'>
         <span>© 2025 Temple of the Feminine Divine 501c3</span>
         <span>PO BOX 218</span>
         <span>Brooks, ME 04921</span>
